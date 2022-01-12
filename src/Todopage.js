fe93cb197e4dbeca0770a3d97995cf15b38099c8
huiwenxchen <97475388+huiwenxchen@users.noreply.github.com>
2022-01-12 12:37:20 -0500

    Create Todopage.js

---
+++ src/Todopage.js
@@ -0,0 +1,16 @@
+import React from "react"
+import TodoList from "./Todo"
+import { PrettyContainer } from "./Home/styles"
+
+const Todo = () => {
+    return(
+        <PrettyContainer>
+            <TodoList />
+        </PrettyContainer>
+    
+    )
+
+    
+}
+
+export default Todo
