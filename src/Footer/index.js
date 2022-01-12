68f0eef3e5032124520a5abb052763467854c6ec
huiwenxchen <97475388+huiwenxchen@users.noreply.github.com>
2022-01-12 12:41:09 -0500

    Create index.js

---
+++ src/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyledFooter } from "./styles"
+
+
+const Footer = () => {
+    return (
+        <StyledFooter>
+        <div className ="footer-container">
+            <section className="footer-info">
+                <p className="footer-text">
+                    Created by Huiwen Chen
+                </p>
+            </section>
+        </div>
+        </StyledFooter>
+    )
+} 
+
+
+
+export default Footer 
