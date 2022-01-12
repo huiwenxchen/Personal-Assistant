f8e75943dc272e482b52f0f3f0bab5c91d3574a7
huiwenxchen <97475388+huiwenxchen@users.noreply.github.com>
2022-01-12 12:37:03 -0500

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,40 @@
+const theme = {
+    color: {
+      beige:'#F2F2F2',
+      black:'#2E282A',
+      lightblue:'#D5E1FF',
+      blue:'#5973B3',
+      lightyellow:'#FFF0CB',
+      yellow:'#FFE8B2',
+    },
+
+    font: {
+      header: {
+        family: 'Montserrat', 
+        weight: 400,
+      },
+      h1:{
+        family: 'Montserrat',
+        weight: 700,
+      },
+      h2:{
+        family: 'Playfair',
+        weight: 400,
+      },
+      h3:{
+        family: 'Lato',
+        weight: 400,
+      },
+      paragraph: {
+        family: 'Lato', 
+        weight: 400,
+      },
+      footer: {
+        family: 'Montserrat',
+        weight: 400,
+      },
+    },
+  }
+  
+
+  export default theme
