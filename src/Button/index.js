9ec8812cd663845410425f71eb821ba79c4d0358
huiwenxchen <97475388+huiwenxchen@users.noreply.github.com>
2022-01-12 12:42:50 -0500

    Create index.js

---
+++ src/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PrettyButton } from './styles'
+
+
+
+const StyledButton = (props) => (
+  <PrettyButton>{props.name} </PrettyButton>
+)
+
+export default StyledButton
